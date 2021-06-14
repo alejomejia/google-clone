d716e700e8013d709c75af55eac7a7e153201fdb
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-06-14 16:32:24 -0500

    add Avatar scss file

--- components/Avatar/index.jsx
+++ components/Avatar/index.jsx
@@ -1,5 +1,7 @@
+import styles from './Avatar.module.scss'
+
 const Avatar = ({ url }) => {
-  return <img className='avatar w-8 h-8 rounded-full' src={url} alt='profile picture' loading='lazy' />
-};
+  return <img className={styles.avatar} src={url} alt='profile picture' loading='lazy' />
+}
 
 export default Avatar
