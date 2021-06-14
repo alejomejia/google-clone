022bbc88c450153c996bbe1fc3dcf68738160655
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-06-14 09:53:28 -0500

    add Head component

---
+++ components/Head/index.jsx
@@ -0,0 +1,14 @@
+import HTMLHead from 'next/head'
+
+const Head = () => {
+  return (
+    <HTMLHead>
+      <title>Google</title>
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link href='https://fonts.googleapis.com/css2?family=Roboto&family=Material+Icons&display=swap' rel='stylesheet' />
+    </HTMLHead>
+  )
+}
+
+export default Head
