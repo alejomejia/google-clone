60cad7941b3c5dbeb44d2d2adc5709a2e9bedec3
Alejandro Mejia <alejomejiacuartas@outlook.com>
2021-06-14 21:50:19 -0500

    update Head component

--- components/Head/index.jsx
+++ components/Head/index.jsx
@@ -1,14 +1,21 @@
 import HTMLHead from 'next/head'
+import PropTypes from 'prop-types'
 
-const Head = () => {
+const Head = ({ title, children }) => {
   return (
     <HTMLHead>
-      <title>Google</title>
+      <title>{title}</title>
       <link rel='icon' href='/favicon.png' />
       <link rel='preconnect' href='https://fonts.gstatic.com' />
       <link href='https://fonts.googleapis.com/css2?family=Roboto&family=Material+Icons&display=swap' rel='stylesheet' />
+      {children}
     </HTMLHead>
   )
+}
+
+Head.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
 }
 
 export default Head
